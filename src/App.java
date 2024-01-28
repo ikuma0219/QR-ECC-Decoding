b7d0a550ae491708b83205f3da4ac1c6fd60c796
ikuma0219 <ikuhig4585@gmail.com>
2024-01-29 03:05:36 +0900

    added sample

--- src/App.java
+++ src/App.java
@@ -1,41 +1,24 @@
-import java.awt.Graphics2D;
-import java.awt.List;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.EnumMap;
-import java.util.Hashtable;
-import java.util.Map;
 
 import javax.imageio.ImageIO;
 
-import java.io.File;
-
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.common.DetectorResult;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import com.google.zxing.qrcode.detector.Detector;
 import com.google.zxing.Binarizer;
 import com.google.zxing.BinaryBitmap;
-import com.google.zxing.ChecksumException;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.FormatException;
 import com.google.zxing.LuminanceSource;
-import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeReader;
 
 public class App {
 	public static void main(String[] args) throws IOException {
 
-		//// QRコードをresourseから複数読み取り,originalとdenoiseを比較していろいろする。
+		// QRコードをdataから複数読み取り,originalとdenoisedを比較
 		// 正しいQRコードと誤ったQRコードのそれぞれについてファイル番号を格納するリストと総数を格納するint
 
 		Analysis analysis = new Analysis();
@@ -54,13 +37,13 @@ public class App {
 		ArrayList<Integer> errorsymbolnum_list = new ArrayList<Integer>();
 
 		// 保存するサンプルの数をカウントする
-		// int samplecount = 0;
+		int samplecount = 0;
 
 		for (int i = 0; i <= 199; i++) {
 			System.out.println(i);
 			// 環境ごとにresourseがあるパスが違うので適宜変更
-			String pathname_original = "C:\\Users\\ikuma\\OneDrive\\デスクトップ\\denoised\\9.1\\" + i + ".png";
-			String pathname_denoise = "C:\\Users\\ikuma\\OneDrive\\デスクトップ\\original\\" + i + ".png";
+			String pathname_original = "data/resourse/original/" + i + ".png";
+			String pathname_denoise = "data/resourse/denoised/10/" + i + ".png";
 
 			File file_original = new File(pathname_original);
 			File file_denoise = new File(pathname_denoise);
@@ -72,68 +55,68 @@ public class App {
 				errormodulenum = analysis.error_module_count(image_original, image_denoise);
 				errorsymbolnum = analysis.error_symbol_count(image_original, image_denoise);
 
-				// if (errorsymbolnum > 5) {
-				// 	//// 誤りシンボルが6以上のQRコードとその誤りシンボルと誤りモジュールの位置を画像として保存
+				if (errorsymbolnum > 5) {
+					//// 誤りシンボルが6以上のQRコードとその誤りシンボルと誤りモジュールの位置を画像として保存
 
-				// 	// ノイズ補正したQRコードを二値化したものも欲しいのでここで用意
-				// 	LuminanceSource source_denoise = new BufferedImageLuminanceSource(image_denoise);
-				// 	Binarizer binarizer_denoise = new HybridBinarizer(source_denoise);
-				// 	BinaryBitmap bitmap_denoise = new BinaryBitmap(binarizer_denoise);
-				// 	BitMatrix bitmatrix_denoise = bitmap_denoise.getBlackMatrix();
-				// 	BufferedImage image_denoise_bin = MatrixToImageWriter.toBufferedImage(bitmatrix_denoise);
+					// ノイズ補正したQRコードを二値化したものも欲しいのでここで用意
+					LuminanceSource source_denoise = new BufferedImageLuminanceSource(image_denoise);
+					Binarizer binarizer_denoise = new HybridBinarizer(source_denoise);
+					BinaryBitmap bitmap_denoise = new BinaryBitmap(binarizer_denoise);
+					BitMatrix bitmatrix_denoise = bitmap_denoise.getBlackMatrix();
+					BufferedImage image_denoise_bin = MatrixToImageWriter.toBufferedImage(bitmatrix_denoise);
 
-				// 	BufferedImage errormodule_place = analysis.error_module_pos(image_original, image_denoise);
-				// 	BufferedImage errorsymbol_place = analysis.error_symbol_pos(image_original, image_denoise);
+					BufferedImage errormodule_place = analysis.error_module_pos(image_original, image_denoise);
+					BufferedImage errorsymbol_place = analysis.error_symbol_pos(image_original, image_denoise);
 
-				// 	// 適宜パスを変更
-				// 	String pathname_denoiseqr = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\errorsample\\denoiseqr\\"
-				// 			+ i + ".png";
-				// 	String pathname_denoisebinqr = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\errorsample\\denoisebinqr\\"
-				// 			+ i + ".png";
-				// 	String pathname_originalqr = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\errorsample\\originalqr\\"
-				// 			+ i + ".png";
-				// 	String pathname_errormodule_place = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\errorsample\\errormodplace\\"
-				// 			+ i + ".png";
-				// 	String pathname_errorsymbol_place = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\errorsample\\errorsymplace\\"
-				// 			+ i + ".png";
-				// 	ImageIO.write(image_denoise, "png", new File(pathname_denoiseqr));
-				// 	ImageIO.write(image_denoise_bin, "png", new File(pathname_denoisebinqr));
-				// 	ImageIO.write(image_original, "png", new File(pathname_originalqr));
-				// 	ImageIO.write(errormodule_place, "png", new File(pathname_errormodule_place));
-				// 	ImageIO.write(errorsymbol_place, "png", new File(pathname_errorsymbol_place));
-				// } else if ((errorsymbolnum <= 5) && (samplecount < 10)) {
-				// 	//// 正しく読み取れたQRコードについてもサンプルをとる
+					// 適宜パスを変更
+					String pathname_denoiseqr = "data/sample/denoiseqr/"
+							+ i + ".png";
+					String pathname_denoisebinqr = "data/sample/denoisebinqr/"
+							+ i + ".png";
+					String pathname_originalqr = "data/sample/originalqr/"
+							+ i + ".png";
+					String pathname_errormodule_place = "data/sample/errormodule_place/"
+							+ i + ".png";
+					String pathname_errorsymbol_place = "data/sample/errorsymbol_place/"
+							+ i + ".png";
+					ImageIO.write(image_denoise, "png", new File(pathname_denoiseqr));
+					ImageIO.write(image_denoise_bin, "png", new File(pathname_denoisebinqr));
+					ImageIO.write(image_original, "png", new File(pathname_originalqr));
+					ImageIO.write(errormodule_place, "png", new File(pathname_errormodule_place));
+					ImageIO.write(errorsymbol_place, "png", new File(pathname_errorsymbol_place));
+				} else if ((errorsymbolnum <= 5) && (samplecount < 10)) {
+					//// 正しく読み取れたQRコードについてもサンプルをとる
 
-				// 	// ノイズ補正したQRコードを二値化したものも欲しいのでここで用意
-				// 	LuminanceSource source_denoise = new BufferedImageLuminanceSource(image_denoise);
-				// 	Binarizer binarizer_denoise = new HybridBinarizer(source_denoise);
-				// 	BinaryBitmap bitmap_denoise = new BinaryBitmap(binarizer_denoise);
-				// 	BitMatrix bitmatrix_denoise = bitmap_denoise.getBlackMatrix();
-				// 	BufferedImage image_denoise_bin = MatrixToImageWriter.toBufferedImage(bitmatrix_denoise);
+					// ノイズ補正したQRコードを二値化したものも欲しいのでここで用意
+					LuminanceSource source_denoise = new BufferedImageLuminanceSource(image_denoise);
+					Binarizer binarizer_denoise = new HybridBinarizer(source_denoise);
+					BinaryBitmap bitmap_denoise = new BinaryBitmap(binarizer_denoise);
+					BitMatrix bitmatrix_denoise = bitmap_denoise.getBlackMatrix();
+					BufferedImage image_denoise_bin = MatrixToImageWriter.toBufferedImage(bitmatrix_denoise);
 
-				// 	BufferedImage errormodule_place = analysis.error_module_pos(image_original, image_denoise);
-				// 	BufferedImage errorsymbol_place = analysis.error_symbol_pos(image_original, image_denoise);
+					BufferedImage errormodule_place = analysis.error_module_pos(image_original, image_denoise);
+					BufferedImage errorsymbol_place = analysis.error_symbol_pos(image_original, image_denoise);
 
-				// 	// 適宜パスを変更
-				// 	String pathname_denoiseqr = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\correctsample\\denoiseqr\\"
-				// 			+ i + ".png";
-				// 	String pathname_denoisebinqr = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\correctsample\\denoisebinqr\\"
-				// 			+ i + ".png";
-				// 	String pathname_originalqr = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\correctsample\\originalqr\\"
-				// 			+ i + ".png";
-				// 	String pathname_errormodule_place = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\correctsample\\errormodplace\\"
-				// 			+ i + ".png";
-				// 	String pathname_errorsymbol_place = "C:\\code\\QRreader\\qrreder\\AI2data\\sampledata8_0\\correctsample\\errorsymplace\\"
-				// 			+ i + ".png";
-				// 	ImageIO.write(image_denoise, "png", new File(pathname_denoiseqr));
-				// 	ImageIO.write(image_denoise_bin, "png", new File(pathname_denoisebinqr));
-				// 	ImageIO.write(image_original, "png", new File(pathname_originalqr));
-				// 	ImageIO.write(errormodule_place, "png", new File(pathname_errormodule_place));
-				// 	ImageIO.write(errorsymbol_place, "png", new File(pathname_errorsymbol_place));
+					// 適宜パスを変更
+					String pathname_denoiseqr = "data/sample/denoiseqr/"
+							+ i + ".png";
+					String pathname_denoisebinqr = "data/sample/denoisebinqr/"
+							+ i + ".png";
+					String pathname_originalqr = "data/sample/originalqr/"
+							+ i + ".png";
+					String pathname_errormodule_place = "data/sample/errormodule_place/"
+							+ i + ".png";
+					String pathname_errorsymbol_place = "data/sample/errorsymbol_place/"
+							+ i + ".png";
+					ImageIO.write(image_denoise, "png", new File(pathname_denoiseqr));
+					ImageIO.write(image_denoise_bin, "png", new File(pathname_denoisebinqr));
+					ImageIO.write(image_original, "png", new File(pathname_originalqr));
+					ImageIO.write(errormodule_place, "png", new File(pathname_errormodule_place));
+					ImageIO.write(errorsymbol_place, "png", new File(pathname_errorsymbol_place));
+				} else if ((errorsymbolnum <= 5) && (samplecount < 10)) {
 
-				// 	samplecount++;
-
-				// }
+					samplecount++;
+				}
 
 				System.out.println("errormodulenum:" + errormodulenum);
 				System.out.println("errorsymbolnum:" + errorsymbolnum);
@@ -194,10 +177,9 @@ public class App {
 		correctcount = correctnum.size();
 		errorcount = errornum.size();
 
-		System.out.println("correctnum:" + correctnum);
+		// System.out.println("correctnum:" + correctnum);
 		System.out.println("correctcount:" + correctcount);
-		System.out.println("errornum:" + errornum);
+		// System.out.println("errornum:" + errornum);
 		System.out.println("errorcount:" + errorcount);
-
 	}
 }
