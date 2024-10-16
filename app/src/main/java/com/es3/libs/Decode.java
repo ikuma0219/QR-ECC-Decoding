1d47878781c01621d442f2e6ed6da5222511c4a7
ikuma0219 <ikuhig4585@gmail.com>
2024-10-16 13:36:30 +0900

    change directory

---
+++ app/src/main/java/com/es3/libs/Decode.java
@@ -0,0 +1,28 @@
+package com.es3.libs;
+
+import java.awt.image.BufferedImage;
+import com.google.zxing.Binarizer;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+
+
+public class Decode {
+    	public static String decodeQRCode(BufferedImage image, String errorSymbol, int i)
+			throws NotFoundException, ChecksumException, FormatException {
+		LuminanceSource source = new BufferedImageLuminanceSource(image);
+		Binarizer binarizer = new HybridBinarizer(source);
+		BinaryBitmap bitmap = new BinaryBitmap(binarizer);
+		QRCodeReader reader = new QRCodeReader();
+		Result result = reader.decode(bitmap);
+		String data = result.getText();
+		return data;
+	}
+    
+}
