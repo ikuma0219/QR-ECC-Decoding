8a063d43fa9e2a00e374b3cf29de33b09c1ba2d2
ikuma0219 <ikuhig4585@gmail.com>
2025-04-14 17:08:49 +0900

    added random

--- app/src/main/java/com/es3/utils/PathManager.java
+++ app/src/main/java/com/es3/utils/PathManager.java
@@ -1,9 +1,9 @@
 package com.es3.utils;
 
 public class PathManager {
-    public static final String ORIGINAL_IMAGE_DIR = "app/data/resourse/fixed/original/";
-    public static final String DENOISED_IMAGE_DIR = "app/data/resourse/fixed/denoised/";
-    public static final String NOISE_LEVEL = "10.6";
+    public static final String ORIGINAL_IMAGE_DIR = "app/data/resourse/10000/original/";
+    public static final String DENOISED_IMAGE_DIR = "app/data/resourse/10000/denoised/random/";
+    public static final String NOISE_LEVEL = "9.6";
 
     public static String getOriginalImagePath(int index) {
         return ORIGINAL_IMAGE_DIR + index + ".png";
