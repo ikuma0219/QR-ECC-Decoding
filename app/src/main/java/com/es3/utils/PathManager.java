5386fbaf7a85b14bdaf0e449babda32d216242f4
ikuma0219 <ikuhig4585@gmail.com>
2025-05-02 14:32:37 +0900

    added QRCodes

--- app/src/main/java/com/es3/utils/PathManager.java
+++ app/src/main/java/com/es3/utils/PathManager.java
@@ -2,8 +2,8 @@ package com.es3.utils;
 
 public class PathManager {
     public static final String ORIGINAL_IMAGE_DIR = "app/data/resourse/ver2/original/";
-    public static final String DENOISED_IMAGE_DIR = "app/data/resourse/ver2/denoised/20000/random/";
-    public static final String NOISE_LEVEL = "10.5";
+    public static final String DENOISED_IMAGE_DIR = "app/data/resourse/ver2/denoised/20000/over10/";
+    public static final String NOISE_LEVEL = "10.3";
 
     public static String getOriginalImagePath(int index) {
         return ORIGINAL_IMAGE_DIR + index + ".png";
