64882b5f4d2e57127355887fb49a871af3141689
ikuma0219 <ikuhig4585@gmail.com>
2025-05-27 17:45:26 +0900

    deleted finderpattern

--- app/src/main/java/com/es3/utils/PathManager.java
+++ app/src/main/java/com/es3/utils/PathManager.java
@@ -3,7 +3,7 @@ package com.es3.utils;
 public class PathManager {
     public static final String ORIGINAL_IMAGE_DIR = "app/data/resourse/scratch/original/";
     public static final String DENOISED_IMAGE_DIR = "app/data/resourse/scratch/denoised/2000/";
-    public static final String NOISE_LEVEL = "500";
+    public static final String NOISE_LEVEL = "800";
 
     public static String getOriginalImagePath(int index) {
         return ORIGINAL_IMAGE_DIR + index + ".png";
